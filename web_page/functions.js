042f2187214799e728c117398059be38bea30209
Utkarsh Bajaj <44445191+utkarshbajaj@users.noreply.github.com>
2021-05-09 21:13:57 +0530

    changed webpage

---
+++ web_page/functions.js
@@ -0,0 +1,35 @@
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+var firestore = firebase.firestore();
+
+
+function myFunction() {
+	var x = document.getElementById("frm1");
+	const handleToSave = x.elements[1].value;
+	const nameToSave = x.elements[0].value;
+	const emailToSave = x.elements[2].value;
+
+	console.log("Handle that user has inserted is " + handleToSave);
+
+	if((nameToSave=="") || (emailToSave=="") || (handleToSave=="")){
+		alert("Please fill all fields!");
+		return;
+	}
+
+	const docRef = firestore.doc("users/" + handleToSave);
+
+	docRef.set({
+		userhandle: handleToSave,
+		username: nameToSave,
+		useremail: emailToSave
+	}).then(function() {
+		console.log("Status saved!");
+	}).catch(function(error) {
+		console.log("Got an error: ", error)
+	});
+
+}
+
+
+
