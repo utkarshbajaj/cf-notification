10bd9350a6b64bd2409d80f2b860149e9f7a8bbb
Utkarsh Bajaj <44445191+utkarshbajaj@users.noreply.github.com>
2021-05-09 21:34:34 +0530

    Updates

---
+++ passwd.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyAbDswr80bHOXsVMWazVQ4Hd88lDrVv_ho",
+	authDomain: "cfrankingupdate-90b5b.firebaseapp.com",
+	databaseURL: "https://cfrankingupdate-90b5b-default-rtdb.firebaseio.com",
+	projectId: "cfrankingupdate-90b5b",
+	storageBucket: "cfrankingupdate-90b5b.appspot.com",
+	messagingSenderId: "971533794427",
+	appId: "1:971533794427:web:a518e3e1d50bc58dd0f6de"
+};
